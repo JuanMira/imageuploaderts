56a94f343b1987a6e44241ff4e3d58b991aff4f1
JuanMira <juanmira20@hotmail.com>
2021-09-07 02:09:18 -0500

    Image upload saved in database

--- src/libs/multer.ts
+++ src/libs/multer.ts
@@ -2,8 +2,8 @@ import multer from "multer";
 import { dirname } from "../vars";
 import fs from "fs";
 import { Params, Query, Request } from "../interfaces/middlewares";
-import path from 'path'
-import Image from '../models/Images'
+import path from "path";
+import Image from "../models/Images";
 
 interface RequestQuery extends Query {}
 
@@ -16,19 +16,28 @@ const storage = multer.diskStorage({
     req: Request<any, RequestQuery, RequestParams>,
     file,
     cb
-  ) {      
-    if (fs.existsSync(`./src/uploads/${req.userId}`)) {        
+  ) {
+    if (fs.existsSync(`./src/uploads/${req.userId}`)) {
       cb(null, `./src/uploads/${req.userId}`);
-    } else {        
-      fs.mkdirSync(`./src/uploads/${req.userId}`,{recursive:true});
+    } else {
+      fs.mkdirSync(`./src/uploads/${req.userId}`, { recursive: true });
       cb(null, dirname + `/uploads/${req.userId}`);
-    }    
+    }
   },
-  filename: function (req:Request<any, RequestQuery, RequestParams>, file, cb) {
+  filename: function (
+    req: Request<any, RequestQuery, RequestParams>,
+    file,
+    cb
+  ) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const filename:string = file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname);
+    const filename: string =
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname);
     cb(null, filename);
-    const image = new Image({name:filename,path:dirname + `/uploads/${req.userId}`})
+    const image = new Image({
+      name: filename,
+      path: dirname + `/uploads/${req.userId}`,
+      userId: req.userId,
+    });
     image.save();
   },
 });
