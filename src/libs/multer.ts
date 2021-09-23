bcfe9c935cb6141f8841e45e328012bc1bda475a
juanmira20 <juanmira720@gmail.com>
2021-09-23 02:10:09 -0500

    refactor upload functions

--- src/libs/multer.ts
+++ src/libs/multer.ts
@@ -12,7 +12,7 @@ interface RequestParams extends Params {
   userId: string;
 }
 
-const storage = multer.diskStorage({
+export const imageStorage = multer.diskStorage({
   destination(req: Request<any, RequestQuery, RequestParams>, file, cb) {
     if (fs.existsSync(`./src/uploads/${req.userId}`)) {
       cb(null, `./src/uploads/${req.userId}`);
@@ -21,38 +21,34 @@ const storage = multer.diskStorage({
       cb(null, dirname + `/uploads/${req.userId}`);
     }
   },
-  filename(req: Request<any, RequestQuery, RequestParams>, file, cb) {
+  async filename(req: Request<any, RequestQuery, RequestParams>, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     const filename: string =
       file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname);
     cb(null, filename);
     const image = new Image({
       name: filename,
-      path: dirname + `/uploads/${req.userId}`,
+      path: `${req.userId}/${filename}`,
       userId: req.userId,
     });
-    image.save();
+    await image.save();
   },
 });
 
 export const avatarStorage = multer.diskStorage({
   async destination(req: Request<any, RequestQuery, RequestParams>, file, cb) {
+    console.log("avatar uploading...")
     if (fs.existsSync(`./src/uploads/${req.userId}/avatar/`)) {
       cb(null, `./src/uploads/${req.userId}/avatar/`);
     } else {
       fs.mkdirSync(`./src/uploads/${req.userId}/avatar/`, { recursive: true });
       cb(null, dirname + `/uploads/${req.userId}/avatar/`);
-      const dest = `${dirname}/uploads/${req.userId}/avatar/${
-        req.userId
-      }.png`;
+      const dest = `${req.userId}/avatar/${req.userId}.png`;
       await User.findByIdAndUpdate(req.userId, { avatarImage: dest });
     }
   },
   async filename(req: Request<any, RequestQuery, RequestParams>, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     const filename: string = req.userId + ".png";
     cb(null, filename);
   },
 });
-
-export default storage;
